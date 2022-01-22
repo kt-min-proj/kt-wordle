7e6290861aa79feb5893a5e0f612c2f8cb9a7d25
Bae Sung Hoon <dan990429@gmail.com>
2022-01-22 13:16:07 +0900

    Add cal.templates

--- cal/views.py
+++ cal/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request) -> HttpResponse:
+    return render(
+        request=request,
+        template_name="calendar.html",
+        context={},
+    )
