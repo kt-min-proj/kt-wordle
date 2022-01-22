a157909541286e7549be53c1fc3351a04df37844
fish895623 <dan990429@gmail.com>
2022-01-22 20:31:58 +0900

    Initial post app

--- cal/urls.py
+++ cal/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("post/", include("cal.post.urls")),
 ]
