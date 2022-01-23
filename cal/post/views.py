62672af44da5baf0177cee72723762b057f81799
fish895623 <dan990429@gmail.com>
2022-01-23 14:45:32 +0900

    Testing

--- cal/post/views.py
+++ cal/post/views.py
@@ -1,6 +1,5 @@
 import json
-from django.core import serializers
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 
