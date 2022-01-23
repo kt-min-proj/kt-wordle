53fc14547d9663400c1ded9eff289ea192eacabd
fish895623 <dan990429@gmail.com>
2022-01-23 21:04:31 +0900

    Use axios to send json

--- cal/views.py
+++ cal/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    print(request.POST.get("date"))
+    print(request.body)
     a = request.POST.get("date")
     return render(
         request=request,
