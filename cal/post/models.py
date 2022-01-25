ee91d241d868bda237fca67f6081328fd0c4d3d7
autoblack <cclauss@users.noreply.github.com>
2022-01-25 02:29:54 +0000

    fixup: Format Python code with Black

--- cal/post/models.py
+++ cal/post/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Calendar(models.Model):
     objects = None
-    date = models.DateField(primary_key='date')
+    date = models.DateField(primary_key="date")
     answer = models.CharField(max_length=20)
 
     class Meta:
