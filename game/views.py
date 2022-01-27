b6f94ed698275b7d84eba645f32ee20a96c23c0c
autoblack <cclauss@users.noreply.github.com>
2022-01-27 04:29:27 +0000

    fixup: Format Python code with Black

--- game/views.py
+++ game/views.py
@@ -7,26 +7,19 @@ from django.shortcuts import render
 from master_user.models import WordleAnswers, WordleDayRanks
 from member.models import WordleUser
 
-#wordle_answer에서 정답받아보내기
+# wordle_answer에서 정답받아보내기
 def mainplay(request):
     n = WordleAnswers.objects.get(date=timezone.now())
     data = n
-    return render(
-        request, 
-        "index/aidle_main.html",
-        {"data" : data}
-    )
+    return render(request, "index/aidle_main.html", {"data": data})
 
-#맞춘사람 wordle_dayRank저장 (시간, userid, 횟수)
+
+# 맞춘사람 wordle_dayRank저장 (시간, userid, 횟수)
 def sendinfo(request):
-    if request.method == 'POST':
-        recorded_at = request.POST.get('recorded_at')
-        user = request.POST.get('user')
-        count = request.POST.get('count')
-        
-        record = WordleDayRanks(
-            recorded_at = recorded_at,
-            user = user,
-            count = count
-        )
+    if request.method == "POST":
+        recorded_at = request.POST.get("recorded_at")
+        user = request.POST.get("user")
+        count = request.POST.get("count")
+
+        record = WordleDayRanks(recorded_at=recorded_at, user=user, count=count)
         record.save()
