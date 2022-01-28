416cdddabf8f939fb656a90c543acb601b6cc93d
fish895623 <dan990429@gmail.com>
2022-01-28 10:08:49 +0900

    포맷변환 삭제

--- cal/post/views.py
+++ cal/post/views.py
@@ -24,7 +24,7 @@ def calendar_view(request):
     json_data = json.loads(request.body)
     _date = datetime.today().strftime("%Y-%m-%d")
 
-    for i in WA.filter(date=timeconvert(json_data["date"])).values("answer"):
+    for i in WA.filter(date=json_data["date"]).values("answer"):
         json_data["answer"] = i["answer"]
         json_data["rank"] = calendar_rank(
             json_data["rank"],
@@ -57,7 +57,7 @@ def calendar_rank(data: dict):
     return data
 
 
-def timeconvert(date):
-    result = datetime.strptime(date, "%Y-%m-%d").strftime("%Y-%m-%d 00:00:00")
-
-    return result
+# def timeconvert(date):
+#     result = datetime.strptime(date, "%Y-%m-%d").strftime("%Y-%m-%d 00:00:00")
+#
+#     return result
